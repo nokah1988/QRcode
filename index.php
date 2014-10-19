59d7951cb381d8e775fbe09cfe0493f71cf8a63e
nokah <nokah@192.168.0.156>
2014-10-19 15:48:16 +0200

    privacy generator

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>QR code</title>
+    </head>
+    <body>
+        <p>here is your QR code!</p>
+            <img src="qrcode.php?text=http://www.vandenhovennokah.be&size=200&padding=10" alt="QR code">
+    </body>
+</html>
